83b9e211221c01bb2784af45e6ea66b793748879
DekoPossas <andrepossaster@gmail.com>
2022-04-08 09:42:30 -0300

    counter done

--- 01-github-explorer/src/components/Counter.jsx
+++ 01-github-explorer/src/components/Counter.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
-function Counter() {
-  const [counter, setCounter] = useState;
+export function Counter() {
+  const [counter, setCounter] = useState(0);
 
   function increment() {
     setCounter(counter + 1);
@@ -9,7 +9,7 @@ function Counter() {
 
   return (
     <div>
-      <h2>0</h2>
+      <h2>{counter}</h2>
       <button onClick={increment} type="button">
         Increment
       </button>
