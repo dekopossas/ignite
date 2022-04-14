084339856ce93acbd07fcc802502ed254a1de257
DekoPossas <andrepossaster@gmail.com>
2022-04-14 00:28:27 -0300

    typescript done

--- 01-github-explorer/src/components/RepositoryList.tsx
+++ 01-github-explorer/src/components/RepositoryList.tsx
@@ -3,7 +3,11 @@ import { useState, useEffect } from 'react';
 
 import '../styles/repositories.scss';
 
-// https://api.github.com/orgs/rocketseat/repos
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
 
 const repository = {
   name: 'unform',
@@ -12,7 +16,7 @@ const repository = {
 };
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
