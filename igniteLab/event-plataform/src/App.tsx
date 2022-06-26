702a3d34ca4e8f208dcf66e208ae57b6cbe50d10
DekoPossas <andrepossaster@gmail.com>
2022-06-25 21:37:57 -0300

    first request

--- igniteLab/event-plataform/src/App.tsx
+++ igniteLab/event-plataform/src/App.tsx
@@ -1,4 +1,34 @@
+import { gql } from '@apollo/client';
+import { useEffect } from 'react';
+import { client } from './lib/apollo';
+
+// Querys
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+
+      teacher {
+        name
+      }
+    }
+  }
+`;
+
 function App() {
+  // Effects
+  useEffect(() => {
+    client
+      .query({
+        query: GET_LESSONS_QUERY,
+      })
+      .then((response) => {
+        console.log(response.data);
+      });
+  }, []);
+
+  // Render
   return <h1 className="text-5xl font-bold">Hello World</h1>;
 }
 
