331c37bf857762f8fdbf4992619fdf713e363e02
DekoPossas <andrepossaster@gmail.com>
2022-06-26 21:47:22 -0300

    webkit font smoothing

--- igniteLab/event-plataform/src/components/Sidebar.tsx
+++ igniteLab/event-plataform/src/components/Sidebar.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <aside className="w-[348px] br-gray-700 p-6 border-l border-gray-600">
+      <span className="font-bold text-2xl pd-6 mb-6 border-b border-gray-500 block">
+        Cronograma de aulas
+      </span>
+    </aside>
   )
 }
 
--- igniteLab/event-plataform/src/pages/Event.tsx
+++ igniteLab/event-plataform/src/pages/Event.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from 'react';
+import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
+import Video from '../components/Video';
 
 function Event() {
   return (
-    <Header />
-  )
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-1">
+        <Video />
+        <Sidebar />
+      </main>
+    </div>
+  );
 }
 
-export default Event
+export default Event;
